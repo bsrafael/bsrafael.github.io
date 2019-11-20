9e2e0af985cd79267b5485bc7a96847c17f02a27
Rafael Silveira <rafaelbs159@gmail.com>
2019-11-20 13:57:10 -0300

    Mobile

--- js/main.js
+++ js/main.js
@@ -1,125 +1,50 @@
 $(document).ready(function () {
 
 
-    // máscaras
-    $('input[name="cep"]').mask("99999-999");
-    $('input[name="cpf"]').mask("999.999.999-99");
-    $('input[name="cnpj"]').mask('00.000.000/0000-00', { reverse: true });
-    var SPMaskBehavior = function (val) {
+    if ($(window).outerWidth() <= 1200) {
+        $('header').append( $('.me') );
+        $('.flexmenu').hide()
+    }
 
-        var mask = "";
-
-        if (val.replace(/\D/g, '').substring(0,4) == "0800")
-        {
-            mask = '0000 000 0000';
-        }
-        else
-        {
-            if (val.replace(/\D/g, '').length === 11)
-            {
-                mask = '(00) 00000-0000';
-            }
-            else
-            {
-                mask = '(00) 0000-00009';
-            }
-        }
-
-        return mask;
-    },
-    spOptions = {
-        onKeyPress: function (val, e, field, options) {
-            field.mask(SPMaskBehavior.apply({}, arguments), options);
-        }
-    };
-    $('input[name="telefone"], input[name="telefone2"]').mask(SPMaskBehavior, spOptions);
-
-    //  controle menu mobile
-    $('#mobile-toggle').click(function () {
-        $('#main-menu').css('right', '0px');
-        $('main').css('filter', 'blur(5px)');
-        $('#fechar-menu').addClass("active");
-        $('html').css('overflow', 'hidden');
-        $('body').css('overflow', 'hidden');
-    });
-
-    $('#fechar-menu').click(function () {
-        $('#main-menu').css('right', '-100%');
-        $('main').css('filter', 'blur(0px)');
-        $('#fechar-menu').removeClass("active");
-        $('html').css('overflow', 'auto');
-        $('body').css('overflow', 'auto');
-    });
-    // fim controle menu mobile
-
-
-    //  controle menu "ativo"
     var prevScrollpos = window.pageYOffset;
     $(window).scroll(function () {
-        if ($(window).scrollTop() > 50) {
+        if ($(window).scrollTop() > 100) {
             $('header').addClass('active');
         }
-        if ($(window).scrollTop() < 50) {
+        if ($(window).scrollTop() < 100) {
             $('header').removeClass('active');
         }
-
-        var currentScrollPos = window.pageYOffset;
-        if ((prevScrollpos > currentScrollPos) || ($(window).scrollTop() < 50)) {
-            $('header').removeClass('headerHidden');
-        } else {
-            $('header').addClass('headerHidden');
-        }
-        prevScrollpos = currentScrollPos;
     });
-    // fim controle menu "ativo"
-
-
-     // controle foco do input
-     $('.form-item__field').on('focus', function(){
-        $(this).parent().addClass('active');
-    })
-
-    $('.form-item__field').on('focusout', function(){
-        if ($(this).val() == '')
-            $(this).parent().removeClass('active');
-    })
-    // fim controle foco do input
-
-    // select2
-    $('select').select2();
-    // fim select2
 
 });
 
-// scroll suave para elemento na página
-function scrollMenu(menu) {
-    $('body, html').animate({ scrollTop: $('#'+menu).offset().top - 50}, 1500);
+var navigating = false;
+function navTo(target) {
+    if ( navigating || $(event.target).hasClass('active') ) {
+        return
+    } 
+    navigating = true;
+    $('.menu-item').removeClass('active');
+    $(event.target).addClass('active');
+    $('.content-tab').fadeOut(function(){
+        $('#' + target).fadeIn(function(){
+            navigating = false;
+        })
+    })
+    if ($(window).outerWidth() <= 1200) {
+        toggleMenu()
+    }
 }
 
-// abre/fecha a busca
-var focused;
-var searchOpen = false;
-function toggleSearch(){
-    $('#search').toggleClass('active');
-    $(document).keyup(function(e){
-        if(e.key === "Escape" && searchOpen) {
-            toggleSearch();
-        }
-    });
+var fading = false
+function toggleMenu() {
+    console.log('toggle')
+    if (fading) return;
+    fading = true;
 
-    if ( $('#search').hasClass('active') ) {
-        $('html').css('overflow', 'hidden');
-        $('body').css('overflow', 'hidden');
-        focused =$(':focus');
-        $('.searchForm__field').focus();
-        searchOpen = true;
+    $('#menu-toggle').toggleClass('active')
+    $('.flexmenu').fadeToggle(function(){
+        fading = false;
+    })
 
-
-    } else {
-        $('html').css('overflow', 'auto');
-        $('body').css('overflow', 'auto');
-        searchOpen = false;
-
-
-    }
 }
